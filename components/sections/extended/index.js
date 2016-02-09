c6ad5be9d5e25ef3cd1afa035f6661eb9f5bfdcd
Mike Kamermans <pomax@nihongoresources.com>
2016-02-09 09:25:37 -0800

    Update index.js

--- components/sections/extended/index.js
+++ components/sections/extended/index.js
@@ -95,8 +95,9 @@ var Explanation = React.createClass({
         the coordinates, but give you freedom over the interval. A great example of this is the <a href="http://levien.com/phd/phd.html">"Spiro"
         curve</a>, which is a curve based on part of a <a href="https://en.wikipedia.org/wiki/Euler_spiral">Cornu Spiral, also
         known as Euler's Spiral</a>. It's a very easthetically pleasing curve and you'll find it in
-        quite a few graphics packages like Illustrator and Inkscape, having even been used in font
-        design (such as for the Inconsolata font).</p>
+        quite a few graphics packages
+        like <a href="https://fontforge.github.io">FontForge</a> and <a href="https://inkscape.org">Inkscape</a>, having
+        even been used in font design (such as for the Inconsolata font).</p>
       </section>
     );
   }
