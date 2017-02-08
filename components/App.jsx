6c4510030adf7097ae1f4c743eb19dd2068243ed
Mike Kamermans <pomax@nihongoresources.com>
2017-02-08 08:52:51 -0800

    Update App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -1,4 +1,11 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var FullArticle = require("./FullArticle.jsx");
-ReactDOM.render(<FullArticle/>, document.getElementById("article"));
+var localized = (
+  <div>
+    <FullArticle/>
+    <script src="https://localize-bezierinfo.herokuapp.com/static/js/jquery-1.11.1.min.js"/>
+    <script src="https://localize-bezierinfo.herokuapp.com/pontoon.js"/>
+  </div>
+);
+ReactDOM.render(localized, document.getElementById("article"));
