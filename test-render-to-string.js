394be9f9a66fc1848dde7a91bef27f4a2e60bb20
Pomax <pomax@nihongoresources.com>
2017-03-15 11:50:59 -0700

    section 6 jp

--- test-render-to-string.js
+++ test-render-to-string.js
@@ -4,6 +4,7 @@ var Server = require("react-dom/server");
 
 var FullArticle = require("./article").FullArticle;
 var article = React.createElement(FullArticle, {});
-var html = Server.renderToString(article);
+//var html = Server.renderToString(article);
+var html = Server.renderToStaticMarkup(article);
 
 fs.writeFileSync("fullhtml.html", html);
