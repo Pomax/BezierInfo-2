b3ad275eceab4cee6a6180ee3bf3b9399cfc474b
Pomax <pomax@nihongoresources.com>
2017-03-26 12:10:47 -0700

    further experiments in generating a pure html/js page, with React as build tool only

--- components/sections/index.js
+++ components/sections/index.js
@@ -1,15 +1,22 @@
 /**
- * This is an ordered list of all sections for the article
- * @type {Object}
+ * This is an ordered list of all sections used in the Bezier primer.
+ *
+ * The ordering you see here reflects the ordering in which sections
+ * are present on the Primer page: do not change them unless there is
+ * a REALLY good reason to =)
+ *
  */
 module.exports = {
   preface: require("./preface"),
+
+  // the basic topic(s) introduction(s)
   introduction: require("./introduction"),
   whatis: require("./whatis"),
   explanation: require("./explanation"),
   control: require("./control"),
   extended: require("./extended"),
 
+  // basic operations
   matrix: require("./matrix"),
   decasteljau: require("./decasteljau"),
   flattening: require("./flattening"),
@@ -17,6 +24,7 @@ module.exports = {
   matrixsplit: require("./matrixsplit"),
   reordering: require("./reordering"),
 
+  // information that can be obtained through analysis
   derivatives: require("./derivatives"),
   pointvectors: require("./pointvectors"),
   components: require("./components"),
@@ -27,33 +35,41 @@ module.exports = {
   inflections: require("./inflections"),
   canonical: require("./canonical"),
 
+  // accurate arc length is hard, yo
   arclength: require("./arclength"),
   arclengthapprox: require("./arclengthapprox"),
   tracing: require("./tracing"),
 
+  // curve intersections
   intersections: require("./intersections"),
   curveintersection: require("./curveintersection"),
 
+  // curve manipulation
   abc: require("./abc"),
   moulding: require("./moulding"),
   pointcurves: require("./pointcurves"),
 
+  // A quick foray into Catmull-Rom splines
   catmullconv: require("./catmullconv"),
   catmullmoulding: require("./catmullmoulding"),
 
+  // "things made of more than on curve"
   polybezier: require("./polybezier"),
-
   shapes: require("./shapes"),
 
+  // curve offsetting
   projections: require("./projections"),
   offsetting: require("./offsetting"),
   graduatedoffset: require("./graduatedoffset"),
 
+  // circle and arc approximation
   circles: require("./circles"),
   circles_cubic: require("./circles_cubic"),
   arcapproximation: require("./arcapproximation"),
 
+  // A quick foray in to B-Spline land
   bsplines: require("./bsplines"),
 
+  // comments come last for obvious reasons
   comments: require("./comments")
 };
