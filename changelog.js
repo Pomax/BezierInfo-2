bc33c23d4e5eb8fcec0eb27d09920fdc7f317883
Pomax <pomax@nihongoresources.com>
2018-08-18 18:49:07 -0700

    Autorebuild (#153)
    * chokidar rebuild watching + changelog update

--- changelog.js
+++ changelog.js
@@ -1,19 +1,20 @@
 module.exports = {
 
   "July 2018": [
-    "Solved a long-standing problem around 3D normals, by implementing and explaining Rotation Minimising Frames.",
-    "Updated the section on curve order raising/lowering, showing how to get a least-squares optimized lower order curve."
+    "Rewrote the 3D normals section, implementing and explaining Rotation Minimising Frames.",
+    "Updated the section on curve order raising/lowering, showing how to get a least-squares optimized lower order curve.",
+    "(Finally) updated 'npm test' so that it automatically rebuilds when files are changed while the dev server is running."
   ],
 
   "June 2018": [
     "Added a section on direct curve fitting.",
     "Added source links for all graphics.",
-    "Added the \"What's new?\" section."
+    "Added this \"What's new?\" section."
   ],
 
   "April 2017": [
     "Added a section on 3d normals.",
-    "Added live-updatign for the social link buttons, so it points to the specific section you're reading."
+    "Added live-updating for the social link buttons, so they always link to the specific section you're reading."
   ],
 
   "February 2017": [
@@ -21,17 +22,17 @@ module.exports = {
   ],
 
   "January 2016": [
-    "Added a section the Bezier interval."
+    "Added a section to explain the Bezier interval."
   ],
 
   "December 2015": [
     "Set up the split repository between BezierInfo-2 as development repository, and bezierinfo as live page.",
-    "Removed the need for client-side LaTeX parsing entirely."
+    "Removed the need for client-side LaTeX parsing entirely, so the site doesn't take a full minute or more to load all the graphics."
   ],
 
   "May 2015": [
-    "Switched over to pure JS rather than Processing using Processing.js",
-    "Added Cardano's algorithm."
+    "Switched over to pure JS rather than Processing-through-Processing.js",
+    "Added Cardano's algorithm for finding the roots of a cubic polynomial."
   ],
 
   "April 2015": [
@@ -60,20 +61,20 @@ module.exports = {
   ],
 
   "April 2013": [
-    "Added a section on poly-Bbeziers.",
+    "Added a section on poly-Beziers.",
     "Added a section on boolean shape operations."
   ],
 
   "March 2013": [
     "First drastic rewrite",
-    "Added the sections on circle approximations.",
-    "Added a section on projecting a point on a curve.",
+    "Added sections on circle approximations.",
+    "Added a section on projecting a point onto a curve.",
     "Added a section on tangents and normals.",
     "Added Legendre-Gauss numerical data tables."
   ],
 
   "October 2011": [
-    "First github commit, based on the pre-Primer webpage that covered the basics of Bezier curves in HTML with Processing.js example."
+    "First commit for the https://pomax.github.io/bezierinfo site, based on the pre-Primer webpage that covered the basics of Bezier curves in HTML with Processing.js examples."
   ]
 
 };
