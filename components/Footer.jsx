d8dffb0cdaaa115fd3021ce66f14507451cd1ec1
Pomax <pomax@nihongoresources.com>
2019-06-11 21:26:18 -0700

    license

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ var Footer = React.createClass({
       <div>
         <footer className="copyright">
           This article is {copyright} to me, Mike "Pomax" Kamermans, but the text, code,
-          and images are <a href="https://github.com/Pomax/bezierinfo/blob/gh-pages/LICENSE.md">almost
+          and images are <a href="https://github.com/Pomax/bezierinfo/tree/master/LICENSE.md">almost
           no rights reserved</a>. Go do something cool with it!
         </footer>
 
