d41d02404c152d6ab189b3825c47fc852b976670
Pomax <pomax@nihongoresources.com>
2020-08-05 21:57:16 -0700

    base url resolution

--- tools/build/generate-lang-switcher.js
+++ tools/build/generate-lang-switcher.js
@@ -6,18 +6,10 @@ export default function generateLangSwitcher(currentLocale, allLocales) {
   return allLocales
     .map((locale) => {
       let link;
-      if (currentLocale === defaultLocale) {
-        if (locale === defaultLocale) {
-          link = `./index.html`;
-        } else {
-          link = `${locale}/index.html`;
-        }
+      if (locale === defaultLocale) {
+        link = `./index.html`;
       } else {
-        if (locale === defaultLocale) {
-          link = `../index.html`;
-        } else {
-          link = `../${locale}/index.html`;
-        }
+        link = `./${locale}/index.html`;
       }
       return `<li><a href="${link}">${localeStrings.localeName[locale]}</a></li>`;
     })
