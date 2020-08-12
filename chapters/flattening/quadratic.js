4e1c844c1d6974e4a57be4371a05daf6e0f4ffc0
Pomax <pomax@nihongoresources.com>
2020-08-11 18:21:07 -0700

    flattening

---
+++ chapters/flattening/quadratic.js
@@ -0,0 +1,45 @@
+setup() {
+  this.steps = 8;
+  this.curve = Bezier.defaultQuadratic(this);
+  setMovable(this.curve.points);
+}
+
+draw() {
+  clear();
+
+  this.curve.drawSkeleton();
+
+  noFill();
+  start();
+   for(let i=0, e=this.steps; i<=e; i++) {
+    let p = this.curve.get(i/e);
+    vertex(p.x, p.y);
+  }
+  end();
+
+  this.curve.drawPoints();
+
+  setFill(`black`);
+  text(`Flattened to ${this.steps} segments`, 10, 15);
+}
+
+onKeyDown() {
+  let key = this.keyboard.currentKey;
+
+  if(key === `ArrowUp`) {
+    this.steps++;
+  }
+
+  if(key === `ArrowDown`) {
+    if(this.steps > 1) this.steps--;
+  }
+
+  redraw();
+}
+
+onMouseMove() {
+  if (this.cursor.down) {
+    this.steps = round( map(this.cursor.y, 0,this.height, 16, 1) );
+  }
+  redraw();
+}
