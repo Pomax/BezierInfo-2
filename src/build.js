d05bee39497decb50cd4fcd3f30433b80081d698
Pomax <pomax@nihongoresources.com>
2020-09-27 10:30:22 -0700

    don't deploy .md files

--- src/build.js
+++ src/build.js
@@ -4,6 +4,10 @@ import { processLocale } from "./build/process-locale.js";
 import { createIndexPages } from "./build/create-index-pages.js";
 import { createNewsPages } from "./build/create-news-pages.js";
 
+// TODO: figure out a way to cleanly indicate which dir is "the main content", needing
+//       to be compiled into the index.html file, and which dirs are "satellite content"
+//       that should get compiled into its own dir.
+
 /**
  * main entry point:
  */
