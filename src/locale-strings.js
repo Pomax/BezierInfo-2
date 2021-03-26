ef11c75b77d38c22c93b6d5372f76d23b65725bf
Pomax <pomax@nihongoresources.com>
2021-03-25 17:16:27 -0700

    bitcoin welcome

--- src/locale-strings.js
+++ src/locale-strings.js
@@ -31,44 +31,83 @@ const localeStringData = {
         If this is your first time here: welcome! Let me know if you were looking for anything
         in particular that the primer doesn't cover over on the <a href="https://github.com/Pomax/BezierInfo-2/issues">issue tracker</a>!
       </p>
+    `,
+    "ru-RU": `
       <p>
-        If this is a resource that you're using for research, or writing your own software,
+        Приветствуем на Основах кривых Безье. Это бесплатное пособие/страница/электронная книга затрагивает математику и
+        программирование кривых Безье, охватывает большое количество тем касающихся прорисовки и работы с этим типом кривых,
+        который, по всей видимости, выскакивает везде: от кривых в Фотошоп-е до временных функций CSS и начертаний Шрифтов.
+      </p>
+      <p>
+        Если вы здесь впервые: салют! Обязательно напишите мне, если вы ищете что-то конкретное, на что не найдете ответ в
+        тексте. <a href="https://github.com/Pomax/BezierInfo-2/issues">трекер</a>!
+      </p>
+    `,
+    "uk-UA": `
+      <p>
+        Ласкаво прошу до Підручника з кривих Безьє. Це безкоштовний вебсайт/електронна книга, що пояснює і математичні,
+        і програмувальні аспекти кривих Безьє, покриваючи широкий спектр областей, які стосуються малювання та роботи з
+        цими кривими. Криві Безьє застосовуються всюди, починаючи з кривих у Photoshop, і закінчуючи функціями пом'якшення
+        (easing function) CSS та описом контурів популярних шрифтів.
+      </p>
+      <p>
+        Якщо ви тут вперше: ласкаво прошу! Напишіть мені <a href="https://github.com/Pomax/BezierInfo-2/issues">сюди</a>,
+        якщо вас цікавить щось, пов'язане з кривими Безьє, чого немає у підручнику.
+      </p>
+      `,
+  },
+
+  donationsHeading: {
+    "en-GB": `Donations and sponsorship`,
+  },
+
+  donationCTA: {
+    "en-GB": `<p>
+        If this is a resource that you're using for research, as work reference, or even writing your own software,
         please consider <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">donating</a>
         (any amount helps) or signing up as <a href="https://www.patreon.com/bezierinfo">a patron on Patreon</a>.
         I don't get paid to work on this, so if you find this site valuable, and you'd like it
         to stick around for a long time to come, a lot of coffee went into writing this over the
         years, and a lot more coffee will need to go into it yet: if you can spare a coffee, you'd
-        be helping keep a resource alive and well!
+        be helping keep a resource alive and well.
+      </p>
+      <p>
+        Also, if you are a company and your staff uses this book as a resource, or you use it as an
+        onboarding resource, then please: consider sponsoring the site! I am more than happy to work
+        with your finance department on sponsorship invoicing and recognition.
       </p>
     `,
     "ru-RU": `
       <p>
-        Приветствуем на Основах кривых Безье. Это бесплатное пособие/страница/электронная книга затрагивает математику и программирование кривых Безье, охватывает большое количество тем касающихся прорисовки и работы с этим типом кривых, который, по всей видимости, выскакивает везде: от кривых в Фотошоп-е до временных функций CSS и начертаний Шрифтов.
-      </p>
-      <p>
-        Если вы здесь впервые: салют! Обязательно напишите мне, если вы ищете что-то конкретное, на что не найдете ответ в тексте. 
-        <a href="https://github.com/Pomax/BezierInfo-2/issues">трекер</a>!
-      </p>
-      <p>
         Ежели сей ресурс помог вам в ваших исследованиях или в написании собственной программы рассмотрите возможность
-        <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">задонатить</a> (любая сума годится) или станьте <a href="https://www.patreon.com/bezierinfo">патроном на Patreon</a>. Мне не платят за эту работу, потому, если нашли что-то для себя нужное на этом сайте и вам бы хотелось дабы существование его было долгим и успешным вот вам мысль: на создание этих страниц ушло много кофе, и уйдет еще больше на их совершенствование; если вы можете помочь с кофе — уже это может стать залогом долгой и счастливой жизни этого ресурса.
+        <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">задонатить</a>
+        (любая сума годится) или станьте <a href="https://www.patreon.com/bezierinfo">патроном на Patreon</a>. Мне не
+        платят за эту работу, потому, если нашли что-то для себя нужное на этом сайте и вам бы хотелось дабы существование
+        его было долгим и успешным вот вам мысль: на создание этих страниц ушло много кофе, и уйдет еще больше на их
+        совершенствование; если вы можете помочь с кофе — уже это может стать залогом долгой и счастливой жизни этого ресурса.
       </p>
     `,
     "uk-UA": `
       <p>
-        Ласкаво прошу до Підручника з кривих Безьє. Це безкоштовний вебсайт/електронна книга, що пояснює і математичні, і програмувальні аспекти
-        кривих Безьє, покриваючи широкий спектр областей, які стосуються малювання та роботи з цими кривими. Криві Безьє застосовуються всюди, починаючи з кривих у Photoshop, і закінчуючи
-         функціями пом'якшення (easing function) CSS та описом контурів популярних шрифтів.
+        Якщо ви використовуєте цей ресурс для наукових досліджень, або пишете власне програмне забезпечення, будь ласка,
+        розгляньте можливість <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">фінансової
+        підтримки</a> (будь-яка сума вітається), або <a href="https://www.patreon.com/bezierinfo">станьте патроном на Patreon</a>.
+        Мені не платять за цю роботу, тому якщо ви вважаєте цей сайт корисним, і хотіли б, щоб він підтримувався протягом
+        тривалого часу, знайте: багато кави потрібно було за ці роки, і ще багато буде потрібно. Тому, якщо можете допомогти
+        з кавою, то можете бути впевнені, що цей ресурс буде підтримуватися ще довго!
       </p>
-      <p>
-        Якщо ви тут вперше: ласкаво прошу! Напишіть мені <a href="https://github.com/Pomax/BezierInfo-2/issues">сюди</a>, якщо вас цікавить щось, пов'язане з кривими Безьє, чого немає у підручнику.
-      </p>
-      <p>
-        Якщо ви використовуєте цей ресурс для наукових досліджень, або пишете власне програмне забезпечення, будь ласка, розгляньте можливість
-        <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">фінансової підтримки</a>
-        (будь-яка сума вітається), або <a href="https://www.patreon.com/bezierinfo">станьте патроном на Patreon</a>.
-        Мені не платять за цю роботу, тому якщо ви вважаєте цей сайт корисним, і хотіли б, щоб він підтримувався протягом тривалого часу, знайте:
-        багато кави потрібно було за ці роки, і ще багато буде потрібно. Тому, якщо можете допомогти з кавою, то можете бути впевнені, що цей ресурс буде підтримуватися ще довго!
+    `,
+  },
+
+  bitcoinHeading: {
+    "en-GB": `Bitcoin donations:`,
+  },
+
+  bitcoinCTA: {
+    "en-GB": `<p>
+        If you prefer to donate via Bitcoin, you can donate either directly to
+        <a class="btclk" href="bitcoin:3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu?label=Primer%20on%20Bezier%20Curves">3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu</a>
+        or use the QR code on the right, if that's the kind of convenience you prefer =)
       </p>
     `,
   },
