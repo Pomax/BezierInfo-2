8a6027aeaa94516f7365f8fc1cba6e5c7a9c7935
Fey <crystaltoffee@hotmail.com>
2022-09-03 14:10:49 +0800

    Update locale-strings.js
    Update zh-cn translation

--- src/locale-strings.js
+++ src/locale-strings.js
@@ -39,14 +39,13 @@ const localeStringData = {
     `,
     "zh-CN": `
       <p>
-        Welcome to the Primer on Bezier Curves. This is a free website/ebook dealing with both
-        the maths and programming aspects of Bezier Curves, covering a wide range of topics
-        relating to drawing and working with that curve that seems to pop up everywhere, from
-        Photoshop paths to CSS easing functions to Font outline descriptions.
+        欢迎来到贝塞尔曲线入门。这是一个免费的网站/电子书，涉及贝塞尔曲线的数学和编程的的部分，涵盖的
+        主题很多，包括绘制和处理那条似乎无处不在的曲线，从 Photoshop 的路径到 CSS 缓动函数，再到字体
+        轮廓描述。
       </p>
       <p>
-        If this is your first time here: welcome! Let me know if you were looking for anything
-        in particular that the primer doesn't cover over on the <a href="https://github.com/Pomax/BezierInfo-2/issues">issue tracker</a>!
+        如果这是你第一次来到这里，那么欢迎你！告诉我任何你想要在这里找到但却没有涵盖的内容，请通过
+        <a href="https://github.com/Pomax/BezierInfo-2/issues">issue tracker</a> 反馈!
       </p>
     `,
     "ru-RU": `
@@ -87,7 +86,7 @@ const localeStringData = {
 
   donationsHeading: {
     "en-GB": `Donations and sponsorship`,
-    "zh-CN": `捐赠和赞助`,
+    "zh-CN": `捐款和赞助`,
     "ru-RU": `Пожертвования и спонсорство`,
     "ko-KR": `기부와 후원`,
   },
@@ -106,6 +105,17 @@ const localeStringData = {
         Also, if you are a company and your staff uses this book as a resource, or you use it as an
         onboarding resource, then please: consider sponsoring the site! I am more than happy to work
         with your finance department on sponsorship invoicing and recognition.
+      </p>
+    `,
+    "zh-CN":`<p>
+        如果将此资源用于你的研究，作为工作参考，甚至是你编写的软件，可以考虑 <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=3BNHGHZAS3DP6&locale.x=en_CA">捐款</a>
+        （任何数额都可）或注册成为 <a href="https://www.patreon.com/bezierinfo">Patreon 上的资助人</a>。
+        我在这方面的工作是没有报酬的，所以如果你觉得这个网站有价值，并且希望它能长期存在下去，那么在过去的几年
+        里，我喝了大量的咖啡来编写这个网站。如果你能捐助出一杯咖啡，你就在帮助保持这个资源的生存下去并良好发展。
+      </p>
+      <p>
+        此外，如果你是一家公司，你的员工使用这本书作为资源，或者你把它作为一个入职培训资源，那么请：考虑赞助此网站！
+        我很高兴与您合作。我非常乐意和财务部门合作，提供赞助发票和表彰。
       </p>
     `,
     "ru-RU": `
@@ -162,6 +172,12 @@ const localeStringData = {
         or use the QR code on the right, if that's the kind of convenience you prefer =)
       </p>
     `,
+    "zh-CN":`<p>
+        如果你想通过比特币捐赠，你可以直接捐赠给
+        <a class="btclk" href="bitcoin:3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu?label=Primer%20on%20Bezier%20Curves">3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu</a>
+        或者使用右边的二维码，如果你倾向于这种便利的方式 =)
+      </p>
+    `,
     "ru-RU": `<p>
         Если вы предпочитаете делать пожертвования через биткойн, вы можете сделать пожертвование либо напрямую на
         <a class="btclk" href="bitcoin:3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu?label=Primer%20on%20Bezier%20Curves">3GY1HbQ2cH9V4xBLnRYdEfc42Nd1ZyjLZu</a>
@@ -190,7 +206,7 @@ const localeStringData = {
 
   langSwitchLabel: {
     "en-GB": `Read this in your own language:`,
-    "zh-CN": `用你的母语阅读这里：`,
+    "zh-CN": `用母语阅读该资源：`,
     "ru-RU": `Читайте на своём языке:`,
     "uk-UA": `Читати рідною мовою:`,
     "ko-KR": `다른 언어로 읽기:`,
@@ -198,6 +214,7 @@ const localeStringData = {
 
   langHelpLabel: {
     "en-GB": `Don't see your language listed, or want to see it reach 100%? <a href="https://github.com/Pomax/BezierInfo-2/wiki/help-localize-the-primer-on-bezier-curves">Help translate this content!</a>`,
+    "zh-CN": `没有看到你的语言被列出，或者想看到它达到100%翻译？ <a href="https://github.com/Pomax/BezierInfo-2/wiki/help-localize-the-primer-on-bezier-curves">帮助翻译本资源！</a>`,
     "ru-RU": `Не нашли свой язык или хотите, чтобы он достиг до 100%? <a href="https://github.com/Pomax/BezierInfo-2/wiki/help-localize-the-primer-on-bezier-curves">Помогите нам с переводом!</a>`,
     "uk-UA": `Не бачите своєї мови у списку або хочете, щоб вона досягла 100%? <a href="https://github.com/Pomax/BezierInfo-2/wiki/help-localize-the-primer-on-bezier-curves">Допоможіть перекласти цей контент!</a>`,
     "ko-KR": `원하는 언어가 없거나, 100%까지 올리고 싶다면 <a href="https://github.com/Pomax/BezierInfo-2/wiki/help-localize-the-primer-on-bezier-curves">내용 번역을 도와주세요!</a>`,
@@ -205,6 +222,7 @@ const localeStringData = {
 
   disabledMessage: {
     "en-GB": `Scripts are disabled. Showing fallback image.`,
+    "zh-CN": `脚本被禁用。显示备用图片。`,
     "ru-RU": `Скрипты отключены. Показываем резервное изображение.`,
     "uk-UA": `Скрипти вимкнено. показує резервний.`,
     "ko-KR": `스크립트가 꺼져 있어 대체 이미지를 대신 표시합니다.`,
@@ -213,7 +231,7 @@ const localeStringData = {
   reset: {
     "en-GB": `reset`,
     "ja-JP": `リセット`,
-    "zh-CN": `重启`,
+    "zh-CN": `重置`,
     "ru-RU": `cбросить`,
     "uk-UA": `скинути`,
     "ko-KR": `초기화`,
@@ -227,6 +245,7 @@ const localeStringData = {
 
   changelogTitle: {
     "en-GB": `What's new?`,
+    "zh-CN": `变更日志`,
     "ru-RU": `Что нового?`,
     "uk-UA": `Що нового?`,
     "ko-KR": `업데이트 내역`,
@@ -234,6 +253,7 @@ const localeStringData = {
 
   toggleLabel: {
     "en-GB": `Toggle changes`,
+    "zh-CN": `隐藏/显示变更日志`,
     "ru-RU": `Показать/скрыть изменения`,
     "uk-UA": `Перемкнути зміни`,
     "ko-KR": `수정사항 확인`,
@@ -259,6 +279,7 @@ const localeStringData = {
 
   mainContentLabel: {
     "en-GB": `Main content`,
+    "zh-CN": `目录`,
     "ru-RU": `Содержание`,
     "uk-UA": `Основний вміст`,
     "ko-KR": `본문`,
@@ -266,6 +287,7 @@ const localeStringData = {
 
   changelogLabel: {
     "en-GB": `What's new`,
+    "zh-CN": `变更日志`,
     "ru-RU": `Что нового`,
     "uk-UA": `Зміни`,
     "ko-KR": `업데이트 내역`,
@@ -273,6 +295,7 @@ const localeStringData = {
 
   changelogDescription: {
     "en-GB": `This primer is a living document, and so depending on when you last look at it, there may be new content. Click the following link to expand this section to have a look at what got added, when, or click through to the <a href="./news">News posts</a> for more detailed updates. (<a href="./news/rss.xml">RSS feed</a> available)`,
+    "zh-CN": `本入门是一份活动文档，因此它可能会有新的内容，这取决于你上次查看的时间。单击以下链接以展开，查看添加的内容、时间，或点击浏览 <a href="./news">News posts</a> 获取更多更新信息。 (<a href="./news/rss.xml">RSS feed</a> 可用)`,
     "ru-RU": `Этот документ живое пособие, в зависимости от даты вашего последнего посещения, может появиться новый материал. Кликайте <a href="./news">здесь</a> для просмотра лога изменений. (также доступен <a href="./news/rss.xml">RSS-канал</a>)`,
     "uk-UA": `Цей підручник постійно розвививається, тож залежно від того, коли ви востаннє його переглядали, тут можуть бути оновлення. Перейдіть за <a href="./news">цим посиланням</a>, щоб побачити, що було додано. (Також доступний <a href="./news/rss.xml">RSS-канал</a>)`,
     "ko-KR": `이 입문서는 "살아 숨쉬는" 문서로, 오랜만에 다시 찾아왔다면 새로운 내용이 추가되었을 수도 있습니다. 아래 링크를 눌러서 무엇이 언제 추가되었는지 확인할 수도 있고, <a href="./news">뉴스 포스트</a>(영문, <a href="./news/rss.xml">RSS 피드</a>도 있습니다)에서 자세한 내역을 확인할 수도 있습니다.`,
